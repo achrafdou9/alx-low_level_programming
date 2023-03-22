d2aa765c5e1839fb00473f067f914d1b94b0e471
achraf <achrafdoufir99@gmail.com>
2023-03-21 17:07:19 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main"
+
+/**
+ * print_alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
