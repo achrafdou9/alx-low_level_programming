5fe4163becf37288ed0dbb13dc1a60051489bb8c
achraf <achrafdoufir99@gmail.com>
2023-03-22 18:32:57 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes value
+ * of an integer.
+ *
+ * @i: nput numbers as an integer.
+ *
+ * return: absolute value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}	
